6eb6d4b182bed34d492e8e46a7cb4d85cbb3555e
muchen <gao201709@gmail.com>
2021-10-25 01:26:19 +0800

    vite base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
+export default defineConfig(({mode}) => {
+  console.log(mode)
+  return {
+    base: mode === 'development' ? '' : '/pikpak',
+    plugins: [vue()]
+  }
 })
