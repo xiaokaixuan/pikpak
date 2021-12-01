49ea0d1606581796686455ddfa1d7dc744466ac3
xiaokaixuan <xiaokaixuan@gmail.com>
2021-12-01 17:56:48 +0800

    邀请信息可配 (#14)

--- src/views/testtest.vue
+++ src/views/testtest.vue
@@ -1,9 +1,12 @@
 <template>
 <button @click="invite">接受邀请</button>
+<br><br>
+<button @click="inviteInfo">我的邀请</button>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios';
+import http from '../utils/axios'
 
   const invite = () => {
     const loginData = JSON.parse(window.localStorage.getItem('pikpakLogin') || '{}')
@@ -17,6 +20,16 @@ import axios from 'axios';
           window.$message.success('恭喜您，您已成功增加' + res.data.invited_days + '天')
         } else {
           window.$message.error('您已经邀请过了')
+        }
+      })
+  }
+  const inviteInfo = () => {
+    http.get('https://api-drive.mypikpak.com/vip/v1/activity/inviteInfo')
+      .then((res:any) => {
+        if(res.data.invited_nums >= 0) {
+          window.$message.success('您已成功邀请' + res.data.invited_nums + '人，' + res.data.add_days + '天')
+        } else {
+          window.$message.error(JSON.stringify(res.data))
         }
       })
   }
