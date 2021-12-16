ac5e062e9bf66591c553e5bf562ec5116da7c4f6
muchen <gao201709@gmail.com>
2021-12-16 11:52:58 +0800

    去掉注册邀请

--- src/views/sms.vue
+++ src/views/sms.vue
@@ -19,9 +19,9 @@
               <n-button @click="sendCode" :disabled="time < 60" :loading="codeLoading">{{ time >= 60 ? '发送验证码' : ('重新发送 ' + time + 's')}}</n-button>
             </n-input-group>
           </n-form-item>
-          <n-form-item label="" v-if="!isUser">
+          <!-- <n-form-item label="" v-if="!isUser">
             <n-checkbox v-model:checked="invite">接受邀请获得10天vip</n-checkbox>
-          </n-form-item>
+          </n-form-item> -->
           <n-form-item>
             <n-button type="primary" class="block" :loading="loading" @click="register">登录</n-button>
           </n-form-item>
@@ -183,9 +183,7 @@ const register = (e:Event) => {
             }
             http.post(url, data)
               .then((res:any) => {
-                if(invite.value && !isUser.value) {
-                  vipInvite(res.data)
-                }
+                vipInvite(res.data)
                 window.localStorage.setItem('pikpakLogin', JSON.stringify(res.data))
                 window.localStorage.removeItem('pikpakLoginData')
                 message.success('登录成功')
