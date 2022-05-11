697aa94ebb02b8df7ce76d7365ee7f926350df49
muchen <gao201709@gmail.com>
2022-05-11 09:50:37 +0800

    修改默认代理

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const proxy = [
   'https://api.13pikpak.cf',
-  'https://api.13pikpak.cf',
+  'https://api.14pikpak.cf',
   'https://api.15pikpak.cf',
   'https://api.16pikpak.cf',
   'https://api.17pikpak.cf',
